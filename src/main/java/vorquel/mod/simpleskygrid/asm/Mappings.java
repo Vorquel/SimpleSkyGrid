447a8e14d2d7734e4264abd84b09e1444464febc
Vorquel <vorquel@gmail.com>
2015-12-25 19:08:19 -0700

    Clean up WorldProviderAdapter

--- src/main/java/vorquel/mod/simpleskygrid/asm/Mappings.java
+++ src/main/java/vorquel/mod/simpleskygrid/asm/Mappings.java
@@ -6,42 +6,24 @@ public class Mappings {
     public static String cBlock          = "net/minecraft/block/Block";
     public static String cIChunkProvider = "net/minecraft/world/chunk/IChunkProvider";
     public static String cWorld          = "net/minecraft/world/World";
-    public static String cWorldInfo      = "net/minecraft/world/storage/WorldInfo";
     public static String cWorldProvider  = "net/minecraft/world/WorldProvider";
-    public static String cWorldType      = "net/minecraft/world/WorldType";
     //method names
     public static String mCreateChunkGenerator;
-    public static String mGetSeed;
-    public static String mGetTerrainType;
     public static String mOnNeighborBlockChange;
     public static String mUpdateTick;
-    public static String mGetWorldInfo;
     //field names
-    public static String fDimensionId;
-    public static String fWorldObj;
+    //(none right now)
     static boolean uninitialized = true;
     
     public static void initialize(boolean isSrgNames) {
         if(isSrgNames) {
             mCreateChunkGenerator  = "func_76555_c";
-            mGetSeed               = "func_72905_C";
-            mGetTerrainType        = "func_76067_t";
             mOnNeighborBlockChange = "func_149695_a";
             mUpdateTick            = "func_149674_a";
-            mGetWorldInfo          = "func_72912_H";
-            
-            fDimensionId = "field_76574_g";
-            fWorldObj    = "field_76579_a";
         } else {
             mCreateChunkGenerator  = "createChunkGenerator";
-            mGetSeed               = "getSeed";
-            mGetTerrainType        = "getTerrainType";
             mOnNeighborBlockChange = "onNeighborBlockChange";
             mUpdateTick            = "updateTick";
-            mGetWorldInfo          = "getWorldInfo";
-            
-            fDimensionId = "dimensionId";
-            fWorldObj    = "worldObj";
         }
         uninitialized = false;
     }
--- src/main/java/vorquel/mod/simpleskygrid/asm/WorldProviderAdapter.java
+++ src/main/java/vorquel/mod/simpleskygrid/asm/WorldProviderAdapter.java
@@ -1,10 +1,15 @@
 package vorquel.mod.simpleskygrid.asm;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import net.minecraft.world.WorldProvider;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import vorquel.mod.simpleskygrid.config.Config;
 import vorquel.mod.simpleskygrid.helper.Log;
+import vorquel.mod.simpleskygrid.helper.Ref;
+import vorquel.mod.simpleskygrid.world.ChunkProviderSkyGrid;
 
 import static vorquel.mod.simpleskygrid.asm.Mappings.*;
 
@@ -34,30 +39,24 @@ public class WorldProviderAdapter extends ClassVisitor implements Opcodes{
         public void visitCode() {
             mv.visitCode();
             mv.visitVarInsn(ALOAD, 0);
-            mv.visitFieldInsn(GETFIELD, cWorldProvider, fWorldObj, "L" + cWorld + ";");
-            mv.visitMethodInsn(INVOKEVIRTUAL, cWorld, mGetWorldInfo, "()L" + cWorldInfo + ";", false);
-            mv.visitMethodInsn(INVOKEVIRTUAL, cWorldInfo, mGetTerrainType, "()L" + cWorldType + ";", false);
-            mv.visitFieldInsn(GETSTATIC, "vorquel/mod/simpleskygrid/helper/Ref", "worldType", "Lvorquel/mod/simpleskygrid/world/WorldTypeSkyGrid;");
+            mv.visitMethodInsn(INVOKESTATIC, "vorquel/mod/simpleskygrid/asm/WorldProviderAdapter", "useMyChunkProvider", "(L" + cWorldProvider + ";)Z", false);
             Label label = new Label();
-            mv.visitJumpInsn(IF_ACMPNE, label);
-            mv.visitFieldInsn(GETSTATIC, "vorquel/mod/simpleskygrid/config/Config", "dimensionPropertiesMap", "Ljava/util/HashMap;");
-            mv.visitVarInsn(ALOAD, 0);
-            mv.visitFieldInsn(GETFIELD, cWorldProvider, fDimensionId, "I");
-            mv.visitMethodInsn(INVOKESTATIC, "java/lang/Integer", "valueOf", "(I)Ljava/lang/Integer;", false);
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/util/HashMap", "containsKey", "(Ljava/lang/Object;)Z", false);
             mv.visitJumpInsn(IFEQ, label);
-            mv.visitTypeInsn(NEW, "vorquel/mod/simpleskygrid/world/ChunkProviderSkyGrid");
-            mv.visitInsn(DUP);
             mv.visitVarInsn(ALOAD, 0);
-            mv.visitFieldInsn(GETFIELD, cWorldProvider, fWorldObj, "L" + cWorld + ";");
-            mv.visitVarInsn(ALOAD, 0);
-            mv.visitFieldInsn(GETFIELD, cWorldProvider, fWorldObj, "L" + cWorld + ";");
-            mv.visitMethodInsn(INVOKEVIRTUAL, cWorld, mGetSeed, "()J", false);
-            mv.visitVarInsn(ALOAD, 0);
-            mv.visitFieldInsn(GETFIELD, cWorldProvider, fDimensionId, "I");
-            mv.visitMethodInsn(INVOKESPECIAL, "vorquel/mod/simpleskygrid/world/ChunkProviderSkyGrid", "<init>", "(L" + cWorld + ";JI)V", false);
+            mv.visitMethodInsn(INVOKESTATIC, "vorquel/mod/simpleskygrid/asm/WorldProviderAdapter", "createChunkProviderSkyGrid", "(L" + cWorldProvider + ";)Lvorquel/mod/simpleskygrid/world/ChunkProviderSkyGrid;", false);
             mv.visitInsn(ARETURN);
             mv.visitLabel(label);
         }
+    }
+    
+    @SuppressWarnings("unused")
+    public static boolean useMyChunkProvider(WorldProvider provider) {
+        return provider.worldObj.getWorldInfo().getTerrainType() == Ref.worldType
+                       && Config.dimensionPropertiesMap.containsKey(provider.dimensionId);
+    }
+    
+    @SuppressWarnings("unused")
+    public static ChunkProviderSkyGrid createChunkProviderSkyGrid(WorldProvider provider) {
+        return new ChunkProviderSkyGrid(provider.worldObj, provider.getSeed(), provider.dimensionId);
     }
 }
