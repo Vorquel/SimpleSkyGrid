c2d4cdfd49f5912e8def55eec19ffdf73c855bcd
Vorquel <vorquel@gmail.com>
2016-05-05 07:06:55 -0700

    Delete outmoded loot system

--- src/main/java/vorquel/mod/simpleskygrid/world/loot/ILootSource.java
+++
@@ -1,9 +0,0 @@
-package vorquel.mod.simpleskygrid.world.loot;
-
-import net.minecraft.inventory.IInventory;
-
-import java.util.Random;
-
-public interface ILootSource {
-    void provideLoot(Random random, IInventory inventory);
-}
